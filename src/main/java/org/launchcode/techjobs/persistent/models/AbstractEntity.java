a5cd71c2d15c7006b492b4856aebd26fce23614c
LightningFrisk <bmxcaleb@gmail.com>
2023-05-25 19:27:34 -0500

    having issues with persistence import, it runs but throws a flag, going to re-import project.

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
-
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotNull
+    @Size(max = 255)
     private String name;
 
     public int getId() {
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotNull
+    @Size(max = 128)
+    String location;
 
+    public Employer() {
+    }
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
